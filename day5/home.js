6209d9c4be2fec7c54e189a742c28050d6ef8023
unknown <sriramprashanth1997@gmail.com>
2018-12-01 16:22:12 +0800

    js1

---
+++ day5/home.js
@@ -0,0 +1,31 @@
+var my_name;
+var my_age;
+var my_birthdate; 
+
+my_name ="Frank";
+my_age ="23";
+my_birthdate ="20/03/1995";
+
+console.log(my_age);
+console.log(my_birthdate);
+console.log(my_name);
+
+var my_address= "JTC LaunchPad"
+var my_test;
+console.log(my_test);
+console.log (typeof(my_name));
+console.log(typeof(my_test));
+console.log(typeof(my_age));
+var firstName;
+var lastName;
+var space;
+firstName ="Nick";
+lastName ="Test";
+space =" ";
+var fullName = firstName + space + lastName
+var fullName1 = firstName + 5 + lastName
+console.log(fullName1);
+var qty = 2;
+var total = 200; 
+var qtyTotal= qty + total;
+console.log(qtyTotal);
